440ec5442b207cad356ee65df749d436b26ace2f
James Putnam <jonathan.putnam86@gmail.com>
2016-10-17 11:55:34 -0600

    making it better

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -11,13 +11,34 @@ class FirebaseService {
   fb.Database _fbDatabase;
   fb.Storage _fbStorage;
   fb.DatabaseReference _fbRefMessages;
+  fb.User user;
+
+  void _authChanged(fb.AuthEvent event) {
+    user = event.user;
+  }
+
+  Future signIn() async {
+  try {
+    await _fbAuth.signInWithPopup(_fbGoogleAuthProvider);
+  }
+  catch (error) {
+    print("$runtimeType::login() -- $error");
+  }
+}
+
+void signOut() {
+  _fbAuth.signOut();
+}
 
   FirebaseService() {
     fb.initializeApp(
-        apiKey: "",
-        authDomain: "",
-        databaseURL: "",
-        storageBucket: ""
+        apiKey: "AIzaSyAApBHjVvZwOpS4rShhyrzYBX__uK8cgaQ",
+        authDomain: "dartchat-1ce85.firebaseapp.com",
+        databaseURL: "https://dartchat-1ce85.firebaseio.com",
+        storageBucket: "dartchat-1ce85.appspot.com"
     );
+    _fbGoogleAuthProvider = new fb.GoogleAuthProvider();
+    _fbAuth = fb.auth();
+    _fbAuth.onAuthStateChanged.listen(_authChanged);
   }
 }
